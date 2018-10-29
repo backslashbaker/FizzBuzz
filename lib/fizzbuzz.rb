c962209b6fd1111c733e81d4306739bfbcb5ed3a
Derek <derek.baker89@gmail.com>
2018-10-29 16:43:32 +0000

    added further tests and completed program for all scenarios

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,14 @@
-def fizzbuzz(number)
-  'fizz'
+def fizzbuzz(n)
+  if
+    (n % 3 == 0) && (n % 5 == 0)
+    'fizzbuzz'
+elsif
+  n % 5 == 0
+  'buzz'
+elsif
+  n % 3 == 0
+  'fizz' 
+else
+  return n
+end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,8 +1,17 @@
 require 'fizzbuzz'
 
-describe "fizzbuzz" do
-  it 'returns "fizz" when passed 3' do
-  expect(fizzbuzz(3)).to eq 'fizz'
+  describe "fizzbuzz" do
+    it 'returns "fizz" when passed a multiple of 3' do
+      expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'returns "buzz" when passed a mulitple of 5'do
+      expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'returns "number" when passed a number not a multiple of 3 or 5' do
+      expect(fizzbuzz(1)).to eq 1
+    end
+    it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+      expect(fizzbuzz(15)).to eq "fizzbuzz"
+    end
 
-end
-end
+    end
